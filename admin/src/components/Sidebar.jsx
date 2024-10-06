cb7d072f28d6c79fec60cd390c8067608630fd42
basavraj1831 <144144279+basavraj1831@users.noreply.github.com>
2024-10-07 01:51:14 +0530

    Update Sidebar.jsx

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -4,19 +4,17 @@ import { assets } from "../assets/assets";
 import { FiList, FiTruck } from "react-icons/fi";
 import { FiPlusCircle } from "react-icons/fi";
 import { FiEdit } from "react-icons/fi";
-import { TbTruckDelivery } from "react-icons/tb";
-import { useState } from "react";
 
 const Sidebar = ({ setShowEdit, showEdit }) => {
   return (
-    <div className="w-[18%] md:w-[25%] lg:w-[18%]  min-h-screen border-r-2 ">
+    <div className="w-[18%] md:w-[25%] lg:w-[18%] min-h-screen border-r-2 ">
       <div className="flex flex-col gap-4 pt-6 pl-[20%] text-[15px]">
         {showEdit ? (
           <NavLink
             to="/edit"
             className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l"
           >
-            <FiPlusCircle className="w-5 h-5" />
+            <FiEdit className="w-5 h-5" />
             <p className="hidden md:block text-xl">Edit Items</p>
           </NavLink>
         ) : (
